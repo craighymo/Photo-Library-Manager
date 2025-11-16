f5a3f91355d6a133614f96433da39ad5989b2bac
craighymo <clh267@scarletmail.rutgers.edu>
2025-11-15 19:02:05 -0500

    PhotoView set up with photos, captions and time

--- src/controller/PhotoView.java
+++ src/controller/PhotoView.java
@@ -3,9 +3,14 @@ package controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import model.Tag;
+import model.Photo;
+import model.Session;
+import java.io.File;
+import java.time.format.DateTimeFormatter;
 
 public class PhotoView {
 
@@ -17,7 +22,38 @@ public class PhotoView {
 
     @FXML
     private void initialize() {
-        status.setText("Photo viewer");
+    	Photo photo = Session.getCurrentPhoto();
+        if (photo == null) {
+            status.setText("No photo selected.");
+            return;
+        }
+
+        // uses filename if theres no caption
+        String caption = photo.getCaption();
+        if (caption == null || caption.isEmpty()) {
+            File file = new File(photo.getFilePath());
+            caption = file.getName();
+        }
+        captionLabel.setText(caption);
+
+
+        if (photo.getDate() != null) {
+            DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            String formatted = photo.getDate().format(format);
+            dateTimeLabel.setText(formatted);
+        } else {
+            dateTimeLabel.setText("");
+        }
+
+        File file = new File(photo.getFilePath());
+        if (file.exists()) {
+            Image image = new Image(file.toURI().toString(), true);
+            imageView.setImage(image);
+            status.setText("Viewing photo");
+        } else {
+            imageView.setImage(null);
+            status.setText("File not found: " + photo.getFilePath());
+        }
     }
 
     @FXML
